3587fb228779cd5e9b33eb006ff2db505cae32ea
Serhii Kot <serhiikot.personal@outlook.com>
2022-02-09 15:32:38 +0200

    new

---
+++ homework20.py
@@ -0,0 +1,19 @@
+'''
+            *
+          * * *
+        * * * * *
+      * * * * * * *
+    * * * * * * * * *
+  * * * * * * * * * * *
+* * * * * * * * * * * * *
+'''
+
+height = int(input('Яка висота трикутника?\n'))
+
+for row in range(height):
+  for cols in range(height*2-1):
+    if row == 0 and cols == height-1 or height-row-1 <= cols <= height+row-1 or row == height-1:
+      print('* ', end=' ')
+    else:
+      print('  ', end=' ')
+  print()
