d1765cf6411b190ad269af3bb6bf0665b479789f
Serhii Kot <serhiikot.personal@outlook.com>
2022-02-12 22:27:52 +0200

    comm

--- Fhinished_Homework/for_testing.py
+++ Fhinished_Homework/for_testing.py
@@ -1,28 +1,6 @@
-d = {}
-print(type(d), d)
-d = dict()
-print(type(d), d)
+str = 'hlhlhl'
+a = 0
 
-d = {
-  1: 'one', 
-  2: 'two'
-}
+a = str[0:1]
 
-print(type(d), d)
-
-lst = [
-  (0, 'zero'),
-  (1, 'one'),
-  (2, 'two')
-]
-
-d = dict(lst)
-print(d[1])
-
-person = {}
-person['name'] = 'Ivan'
-person['surname'] = 'Ivanov'
-person['age'] = 35
-person['chilren'] = ['petr','olga']
-person['pets'] = {'dog': 'sharik', 'cat': 'marsik'}
-# print(person)
+print(a)
