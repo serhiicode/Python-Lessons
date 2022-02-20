045c010873d1e7be884abf8be826c2cb98c2da56
Serhii Kot <serhiikot.personal@outlook.com>
2022-02-20 13:04:53 +0200

    new hm

--- homework26.py
+++ homework26.py
@@ -1,9 +1,12 @@
-'''
-Написать функцию is_year_leap, принимающую 1 аргумент — номер года, и возвращающую True, если год високосный, и False иначе.
-'''
+year_input = int(input('Введіть рік, котрий ви бажаєте перевірити.\n'))
 
-year = int(input('Введіть рік, котрий ви бажаєте перевірити.\n'))
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+def is_year_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
+
+if is_year_leap(year_input) == True:
     print("Це високосний рік.")
 else:
     print("Цей рік не високосний.")
